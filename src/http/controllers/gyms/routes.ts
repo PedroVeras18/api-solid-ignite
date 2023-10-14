2cb002b71717c107167bf30c7d42230bd563c1f6
Pedro Veras <veraspedro1608@gmail.com>
2023-10-14 15:15:48 -0300

    feat: Controllers & Testes E2E: Controller de criação de academia

---
+++ src/http/controllers/gyms/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
+
+export async function gymRoutes(app: FastifyInstance) {
+    app.addHook('onRequest', verifyJWT)
+
+    
+}
