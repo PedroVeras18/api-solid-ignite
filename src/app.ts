6e30625de275a2c8200f83c60811b43b51ca8eb0
Pedro Veras <veraspedro1608@gmail.com>
2023-10-15 12:36:18 -0300

    feat: Refresh Token & RBAC: Estratégia de refresh de token

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 
 import { env } from "./env";
 import { ZodError } from "zod";
@@ -11,7 +12,16 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false,
+    },
+    sign: {
+        expiresIn: '10m',
+    },
 })
+
+app.register(fastifyCookie)
 
 app.register(gymsRoutes)
 app.register(userRoutes)
